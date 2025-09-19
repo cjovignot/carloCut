1938edce9856b590dfbca776ea4b2d4029e21aab
cjovignot <cosme.jovignot@epitech.eu>
2025-09-19 12:45:14 +0200

    [STABLE] Local [STABLE] Vercel + [WIP] navBar mobile

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -50,7 +50,7 @@ export function Navbar() {
 
       {/* Mobile Navbar */}
       <div className="fixed bottom-0 left-0 w-full bg-white border-t border-gray-200 shadow-md md:hidden">
-        <div className="flex h-16">
+        <div className="grid h-12 grid-cols-3">
           {navItems.map((item) =>
             item.path ? (
               <Link
