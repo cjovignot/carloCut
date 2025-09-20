8a2305b1959dcc55f588222308f7ae5b00290e39
cjovignot <cosme.jovignot@epitech.eu>
2025-09-21 01:19:10 +0200

    Update email.ts

--- api/routes/email.ts
+++ api/routes/email.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import nodemailer from "nodemailer";
-import { authenticate, AuthRequest } from "../middleware/auth";
+import { authenticate, AuthRequest } from "../middleware/auth.js";
 
 const router = express.Router();
 
