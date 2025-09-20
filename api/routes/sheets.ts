5a63a9e40f341b9cfd82b6fc6e6e03bc61e6628e
cjovignot <cosme.jovignot@epitech.eu>
2025-09-21 01:23:55 +0200

    Update sheets.ts

--- api/routes/sheets.ts
+++ api/routes/sheets.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import Project from "../models/Project";
-import { authenticate, AuthRequest } from "../middleware/auth";
+import Project from "../models/Project.js";
+import { authenticate, AuthRequest } from "../middleware/auth.js";
 
 const router = express.Router();
 
