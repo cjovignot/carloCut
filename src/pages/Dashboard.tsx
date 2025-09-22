22d38a151a4ce07e03419f5c6689ca4b52508ed1
cjovignot <cosme.jovignot@epitech.eu>
2025-09-22 12:38:53 +0200

    test preview

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -68,7 +68,7 @@ export function Dashboard() {
   return (
     <div className="px-4 py-8 pb-16 mx-auto max-w-7xl sm:px-6 lg:px-8">
       <div className="mb-8">
-        <h1 className="text-3xl font-bold text-gray-900">Tableau de bord</h1>
+        <h1 className="text-3xl font-bold text-gray-900">Tableau de bord PREVIEW</h1>
         {/* <p className="mt-2 text-gray-600">
           Overview of your sheet metal ordering system
         </p> */}
