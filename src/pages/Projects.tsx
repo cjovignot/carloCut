3ee0f2878134e648caf04eb893f34ab891fe37d5
cjovignot <cosme.jovignot@epitech.eu>
2025-09-27 19:22:30 +0200

    [WIPUI] WIP themes classes

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -135,7 +135,7 @@ export function Projects() {
           </p>
         </div>
       ) : (
-        <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+        <div className="grid py-3 grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
           {filteredProjects.map((project) => (
             <div
               key={project._id}
