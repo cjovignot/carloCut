e19cd151b04cf27bfeff73fad94f342879bfd947
cjovignot <cosme.jovignot@epitech.eu>
2025-10-01 12:33:11 +0200

    [MAJ] card backgroundColor

--- src/services/useApplyTheme.ts
+++ src/services/useApplyTheme.ts
@@ -185,7 +185,7 @@ export function generateThemeVars(
   const navbar_text = getBestTextColor(navbar_bg);
 
   const card_bg =
-    mode === "light" ? shadeColor(primary, 0.7) : shadeColor(app_bg, 0.05);
+    mode === "light" ? shadeColor(primary, 0.2) : shadeColor(app_bg, 0.05);
   const card_text = getBestTextColor(card_bg);
 
   const action_bg = primary;
